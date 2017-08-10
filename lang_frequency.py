79ccae814b853fed895101cf295d09065094ac79
Maxim Shafarostov <capestiff@ya.ru>
2017-08-10 13:03:05 +0300

    refactoring on variables names

--- lang_frequency.py
+++ lang_frequency.py
@@ -8,8 +8,8 @@ def load_data(filepath):
     return data
 
 
-def get_most_frequent_words(text):
-    list_of_words = re.findall('[^\W_]+', text, re.UNICODE)
+def get_most_frequent_words(text_data):
+    list_of_words = re.findall('[^\W_]+', text_data, re.UNICODE)
     words_count = {}
     for item in list_of_words:
         words_count[item] = list_of_words.count(item)
@@ -32,9 +32,9 @@ if __name__ == '__main__':
     while not is_txt_file(path_to_file):
         path_to_file = input('Input the path and (or) filename of a text file: ')
 
-    text = load_data(path_to_file).lower()
+    text_data_from_file = load_data(path_to_file).lower()
 
-    top_ten_words = get_most_frequent_words(text)[:10]
+    top_ten_words = get_most_frequent_words(text_data_from_file)[:10]
 
     print('\n10 most frequent words in the file \'{}\':'.format(path_to_file))
     for word in top_ten_words:
