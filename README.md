9ecdcc5aad8dab956c6409267b9c1a2d681fae9d
Maxim Shafarostov <capestiff@ya.ru>
2017-08-10 12:55:54 +0300

    first solution

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The program asks user for the path to a text file on input and prints 10 most frequent words in the file.
+
+# How to start on Linux/Windows
+
+```
+$ python lang_frequence.py  # maybe you should use python3 instead of python due to your system dependencies
+Input the path and (or) filename of a text file: poe_dream.txt
+
+10 most frequent words in the file 'poe_dream.txt'
+a	12
+dream	7
+the	6
+i	6
+in	5
+or	4
+within	4
+that	3
+not	3
+is	3
+```
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,41 @@
+import re
+from pathlib import Path
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as file:
+        data = file.read()
+    return data
 
 
 def get_most_frequent_words(text):
-    pass
+    list_of_words = re.findall('[^\W_]+', text, re.UNICODE)
+    words_count = {}
+    for item in list_of_words:
+        words_count[item] = list_of_words.count(item)
 
+    return sorted(words_count.items(), key=lambda x: x[1], reverse=True)
+
+
+def is_txt_file(str_input):
+    if not str_input:
+        return False
+    elif Path(str_input).is_file() and str_input.lower().endswith('.txt'):
+        return True
+    else:
+        print('Please, verify the path and filename')
+        return False
 
 if __name__ == '__main__':
-    pass
+    path_to_file = None
+
+    while not is_txt_file(path_to_file):
+        path_to_file = input('Input the path and (or) filename of a text file: ')
+
+    text = load_data(path_to_file).lower()
+
+    top_ten_words = get_most_frequent_words(text)[:10]
+
+    print('\n10 most frequent words in the file \'{}\':'.format(path_to_file))
+    for word in top_ten_words:
+        print('{}\t{}'.format(word[0], word[1]))
